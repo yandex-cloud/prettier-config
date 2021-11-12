9dc527d3365afee16344841cd44e0e59077c34ce
VCS Migration <vcs-migration-no-reply@yandex-team.ru>
2021-11-12 16:59:25 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# @yandex-cloud/prettier-config
+
+## Install
+```
+npm install --save-dev prettier @yandex-cloud/prettier-config
+```
+
+## Usage
+Add `.prettierrc.js` file in the project root with the following content:
+
+```js
+module.exports = require("@yandex-cloud/prettier-config");
+```
---
+++ index.js
@@ -0,0 +1,21 @@
+module.exports = {
+    bracketSpacing: false,
+    tabWidth: 4,
+    printWidth: 100,
+    singleQuote: true,
+    trailingComma: 'all',
+    overrides: [
+        {
+            files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+            options: {
+                parser: 'typescript',
+            },
+        },
+        {
+            files: ['*.md', '*.json', '*.yaml', '*.yml'],
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
+};
